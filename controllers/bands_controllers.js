91e3f4ea0fd0bce8922e7a90f412b2754c7e5be7
Saad <educationalmindz@gmail.com>
2024-03-22 15:55:03 -0400

    completed music tour api.

--- controllers/bands_controllers.js
+++ controllers/bands_controllers.js
@@ -1,15 +1,46 @@
 const bands = require('express').Router();
-const { Band } = require('../models');
+const { Band, MeetGreet, SetTime, Event, Sequelize } = require('../models');
 
 bands.get('/', async (req, res) => {
     const bandName = req.query.name; // Assuming the band name is provided as a query parameter
-    const band = await Band.findOne({ where: { name: bandName } });
+    const eventName = req.query.event; // Assuming the event name is provided as a query parameter
 
-    if (band) {
-        res.send(band);
-    } else {
-        res.status(404).send('Band not found');
+    try {
+        let includeModels = [
+            { model: MeetGreet, as: 'meet_greets', include: { model: Event, as: 'event' } },
+            { model: SetTime, as: 'set_times', include: { model: Event, as: 'event' } },
+        ];
+
+        if (eventName) {
+            includeModels = [
+                {
+                    model: MeetGreet,
+                    as: 'meet_greets',
+                    include: { model: Event, as: 'event', where: { name: { [Sequelize.Op.like]: `%${eventName}%` } } },
+                },
+                {
+                    model: SetTime,
+                    as: 'set_times',
+                    include: { model: Event, as: 'event', where: { name: { [Sequelize.Op.like]: `%${eventName}%` } } },
+                },
+            ];
+        }
+
+        const band = await Band.findOne({
+            where: { name: bandName },
+            include: includeModels,
+        });
+
+        if (band) {
+            res.send(band);
+        } else {
+            res.status(404).send('Band not found');
+        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
     }
 });
 
 module.exports = bands;
+
